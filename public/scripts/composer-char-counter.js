d6746e92d0ae46d91cbe9265c1d67457ff6adc1d
Ammar <ammarmasud@gmail.com>
2017-02-21 22:35:51 +0000

    tweet html and css created

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+
+$(function() {
+  countingText = function() {
+    let remaining = 140 - $(this).val().length
+    let counter = $(this).parent().children('span.counter');
+    counter.text(remaining);
+    if (Number(remaining) < 0) {
+      counter.addClass('red-text');
+    } else {
+      counter.removeClass('red-text');
+    }
+  }
+  $('.new-tweet textarea').on('keyup',countingText);
+  $('.new-tweet textarea').on('keypress',countingText);
+});
